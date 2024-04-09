e8a4afbf6d7f89c1672b81abcf0c3fd593afe796
TRAN_Thanh <montblanc670000@gmail.com>
2024-04-09 21:15:47 +0200

    Correction erreur Wave de alt modal

--- assets/maugallery.js
+++ assets/maugallery.js
@@ -212,7 +212,7 @@
                                 ? '<div class="mg-prev" style="cursor:pointer;position:absolute;top:50%;left:-15px;background:white;"><</div>'
                                 : '<span style="display:none;" />'
                             }
-                            <img class="lightboxImage img-fluid"/>
+                            <img class="lightboxImage img-fluid" alt="Contenu de l'image affichée dans la modale au clique"/>
                             ${
                               navigation
                                 ? '<div class="mg-next" style="cursor:pointer;position:absolute;top:50%;right:-15px;background:white;}">></div>'
